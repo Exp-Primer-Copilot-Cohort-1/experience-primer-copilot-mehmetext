8a0fdc447ce2cddd4e80e67af03aae4283f142b9
Mehmet <44941107+mehmetext@users.noreply.github.com>
2024-06-10 18:18:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,9 +1,30 @@
-//create web server
-// var http = require('http');
-// var server = http.createServer(function(req, res) {
-//     res.writeHead(200, {
-//         'Content-Type': 'text/plain'
-//     });
-//     res.end('Hello World\n');
-// });
-// server.listen(3000, '
+// Create web server
+// Run: node comments.js
+// Access: http://localhost:3000/comments
+
+// Import modules
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+// Create web server
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Define route
+app.get('/comments', function (req, res) {
+  fs.readFile('comments.json', function (err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
+});
+
+// Start server
+app.listen(3000, function () {
+  console.log('Server is running on http://localhost:3000');
+});
